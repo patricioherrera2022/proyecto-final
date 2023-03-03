9d82ebd684f594b69c8746df9a4dcfb5966199d8
patricioherrera2022 <herrerapatricio281@gmail.com>
2023-03-03 18:24:14 -0300

    actualizar

--- data/stock.js
+++ data/stock.js
@@ -1,10 +1,68 @@
-const servicio=
+const productos =
 [
     {
-    id: 1,
-    nombre: "String",
-    tipo: "String",
-    desc: "String",
-    imagen: `../images/des.png`,
-    meses: 1
-}]
+        id: 1,
+        nombre: "Buzo 1",
+        tipo: "buzo",
+        desc: "Un buzo que re va con vos",
+        precio: 1200,
+        talle: "L",
+        imagen: '../public/images/buzo.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Remera 1",
+        tipo: "remera",
+        desc: "Una remera que re va con vos",
+        precio: 500,
+        talle: "L",
+        imagen: '../public/images/remera.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Camisa 1",
+        tipo: "camisa",
+        desc: "Una camisa que re va con vos",
+        precio: 900,
+        talle: "S",
+        imagen: '../public/images/camisa.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Pantalon 1",
+        tipo: "pantalon",
+        desc: "Una pantalon que re va con vos",
+        precio: 1600,
+        talle: "L",
+        imagen: '../public/images/pantalon.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Buzo 2",
+        tipo: "buzo",
+        desc: "Un buzo que re va con vos",
+        precio: 1200,
+        talle: "L",
+        imagen: '../public/images/buzo.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Remera 2",
+        tipo: "remera",
+        desc: "Una remera que re va con vos",
+        precio: 500,
+        talle: "L",
+        imagen: '../public/images/remera.jpg',
+        cantidad:1
+    }
+];
